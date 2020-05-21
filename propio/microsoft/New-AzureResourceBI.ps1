8bfcd58e5200c858354112dfa90941eeee020ddc
Rodolfo Castelo <rodolfo.castelo@outlook.com>
2020-05-20 23:23:57 -0500

    commit

--- propio/microsoft/New-AzureResourceBI.ps1
+++ propio/microsoft/New-AzureResourceBI.ps1
@@ -1,10 +1,19 @@
+<################################################################################################
+
+Author: Rodolfo Castelo Méndez
+Versión: 1.0
+Required Modules:
+    AzTable
+    Az
+
+################################################################################################>
+
 <#region de inicio de sesión y datos
 
 Connect-AzAccount 
 $TNT_ID = "fca6d03e-0144-4abb-9215-05ebbce29cb0"
 $OUT_TBL_CNN = "DefaultEndpointsProtocol=https;AccountName=azrsrcbi001;AccountKey=YrHybuuJJJiskDFLcjGXjR/4s+44b0fA5lo0/xj+GFXQoBjd55dgET0KkaLLC06bL7tIWQq8QthmhpC+EoJCXQ==;EndpointSuffix=core.windows.net"
 $OUT_TBL_CTX = New-AzStorageContext -ConnectionString $OUT_TBL_CNN
-$OUT_INT_TBL_NAM = "resourcestableurl"
 $ErrorActionPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 $InformationPreference = "SilentlyContinue"
@@ -21,21 +30,35 @@ foreach($SUB in $COR_AZ_SUB_ALL){
     #region selección de subscripción y recursos
     
     Select-AzSubscription -Subscription $SUB.SubscriptionId
-    $DB_AZ_SUB_INF = Get-AzSubscription -SubscriptionId $SUB.SubscriptionId | Select-Object *
     $DB_AZ_RES_ALL = Get-AzResource | Select-Object * | Sort-Object Type
 
     #endregion selección de subscripción y recursos
 
-    #region de preparación de tabla de accesos a tabla de recursos
+    #region de preparación de tablas maestras
 
-    $OUT_DB_TBL_ACC =  Get-AzStorageTable -Context $OUT_TBL_CTX -Name $OUT_INT_TBL_NAM  # Tabla general de acceso a tabla de recursos
+    $OUT_DB_TBL_ACC =  Get-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterresourcestableurl"
     if(!$OUT_DB_TBL_ACC){
-        $OUT_DB_TBL_ACC = New-AzStorageTable -Context $OUT_TBL_CTX -Name $OUT_INT_TBL_NAM  # Tabla general de acceso a tabla de recursos
+        $OUT_DB_TBL_ACC = New-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterresourcestableurl"
+    }
+
+    $OUT_DB_TBL_SUB =  Get-AzStorageTable -Context $OUT_TBL_CTX -Name "amastersubscription"
+    if(!$OUT_DB_TBL_SUB){
+        $OUT_DB_TBL_SUB = New-AzStorageTable -Context $OUT_TBL_CTX -Name "amastersubscription"
+    }
+
+    $OUT_DB_TBL_RSG =  Get-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterresourcegroup"
+    if(!$OUT_DB_TBL_RSG){
+        $OUT_DB_TBL_RSG = New-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterresourcegroup"
+    }
+
+    $OUT_DB_TBL_REG =  Get-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterregions"
+    if(!$OUT_DB_TBL_REG){
+        $OUT_DB_TBL_REG = New-AzStorageTable -Context $OUT_TBL_CTX -Name "amasterregions"
     }
     
-    #endregion de preparación de tabla de accesos a tabla de recursos
+    #endregion de preparación de tablas maestras
     
-    #region provisionamiento de tablas de recursos
+    #region provisionamiento de tablas de acceso de recursos
     $DB_AZ_RES_TYP  = @()
     ($DB_AZ_RES_ALL | Select-Object Type -Unique) | ForEach-Object { $DB_AZ_RES_TYP += $_.Type.Substring(0,$_.Type.IndexOf("/"))}
     $DB_AZ_RES_TYP = $DB_AZ_RES_TYP | Select-Object -Unique
@@ -58,10 +81,31 @@ foreach($SUB in $COR_AZ_SUB_ALL){
                 "Context" = $FOR_INT_01.Context.ConnectionString
             } | Out-Null
 
+        $FOR_INT_00 = $null
         $FOR_INT_01 = $null
     }
-    #endregion provisionamiento de tablas de recursos
+    #endregion provisionamiento de tablas de acceso de recursos
     
+    #region informacion de las subscripciones
+    
+    foreach($SUB in $COR_AZ_SUB_ALL){
+        Add-AzTableRow `
+        -UpdateExisting `
+        -Table $OUT_DB_TBL_SUB.CloudTable `
+        -PartitionKey $SUB.TenantId `
+        -RowKey $SUB.SubscriptionId `
+        -Property @{
+            "Name" = $SUB.Name;
+            "State" = $SUB.State;
+            "Environment" = ($SUB.ExtendedProperties | ConvertTo-Json | ConvertFrom-Json).Environment
+        } | Out-Null
+    }
+
+    #endregion informacion de las subscripciones
+
+    #region informacion de las regiones empleadas
+    $DB_AZ_RES_ALL | Select-Object Location -Unique
+
 
 }
 
